e7739ecb7a3e297619dc14a44cfb12630f1752b6
Rodrigo Oliveira <r.oliveira.95@hotmail.com>
2024-05-08 10:35:17 -0300

    JPA repository, dependency enjection, database seeding

---
+++ src/main/java/com/oliveiradev/course/config/TestConfig.java
@@ -0,0 +1,26 @@
+package com.oliveiradev.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.oliveiradev.course.entities.User;
+import com.oliveiradev.course.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Rodrigo Ol", "ro@gmail.com", "99999888888", "123456");
+        User u2 = new User(null, "Fernanda Bzz", "fe@gmail.com", "8888889999", "654321");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}
---
+++ src/main/java/com/oliveiradev/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.oliveiradev.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.oliveiradev.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
