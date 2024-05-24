141be883bae3f46022e0f79cf29509173e1743fc
Rodrigo Oliveira <r.oliveira.95@hotmail.com>
2024-05-24 15:24:07 -0300

    Fix class OrdemItemRepository

--- src/main/java/com/oliveiradev/course/repositories/OrderItemRepository.java
+++ src/main/java/com/oliveiradev/course/repositories/OrderItemRepository.java
@@ -2,7 +2,8 @@ package com.oliveiradev.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.oliveiradev.course.entities.OrderItem;
+import com.oliveiradev.course.entities.pk.OrderItemPk;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk> {
 
 }
